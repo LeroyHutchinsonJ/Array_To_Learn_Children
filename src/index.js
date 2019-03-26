d677513bc6ba1dec62386c12ae3c2787c95a32fa
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-03-26 11:35:09 -0400

    Three Components
    Created Three components to exercise use of children arrays, one component only returns the first child of an element, the next only returns the last, and the final component only returns the amount of children that is requested

--- src/index.js
+++ src/index.js
@@ -5,15 +5,36 @@ import "./styles.css";
 
 function App() {
   return (
-    <FirstChildOnly>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </FirstChildOnly>
+    <div className="App">
+      <Head number={1}>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <h3>Good shit</h3>
+      </Head>
+    </div>
   );
 }
 var FirstChildOnly = ({ children }) => {
-  var firstchild = React.Children.toArray(children);
-  return <div>{firstchild[0]}</div>;
+  var childArray = React.Children.toArray(children);
+  return <div>{childArray[0]}</div>;
+};
+
+var LastChildOnly = ({ children }) => {
+  var childArray = React.Children.toArray(children);
+  var lastChild = childArray.length - 1;
+  return <div>{childArray[lastChild]}</div>;
+};
+
+var Head = ({ number, children }) => {
+  var childArray = React.Children.toArray(children);
+
+  var firstNumbers = new Array();
+
+  for (var a = 0; a < number; a++) {
+    firstNumbers[a] = childArray[a];
+  }
+
+  return <div>{firstNumbers}</div>;
 };
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
